d257f0887959b680f06dd4ad7aaaae086800a067
Kavinraj VP <kavinrajvp@gmail.com>
2026-07-26 04:50:07 +0530

    Login moved new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +7,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  formGroup!: FormGroup;
 
-  ngOnInit() {
-    this.formGroup = new FormGroup({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
-      keepSignedIn: new FormControl<boolean>(false)
-    });
-  }
-
-  onSubmit() {
-    if (this.formGroup.invalid) return;
-    console.log(this.formGroup.value);
-  }
+  ngOnInit() { }
+ 
 }
